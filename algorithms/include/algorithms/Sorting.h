58a2b108eafb65e23ae0d160356f6d63bd4e5d08
ubuntu <ajanani85@gmail.com>
2019-01-03 09:30:02 -0800

    selection sort

--- algorithms/include/algorithms/Sorting.h
+++ algorithms/include/algorithms/Sorting.h
@@ -8,6 +8,9 @@
 #ifndef ALGORITHMS_ALGORITHMS_INCLUDE_ALGORITHMS_SORTING_H_
 #define ALGORITHMS_ALGORITHMS_INCLUDE_ALGORITHMS_SORTING_H_
 
+#include <iostream>
+#include <vector>
+
 namespace aj
 {
 class Sort
@@ -18,13 +21,33 @@ public:
 	Sort();
 	~Sort();
 
+	template<class T>
+	static void print(T* arr, int n)
+	{
+		for(int i = 0; i < n; ++i)
+		{
+			std::cout << " " << arr[i];
+		}
+		std::cout << std::endl;
+	}
+
+	template<class T>
+	static void print(std::vector<T> &arr)
+	{
+		for(int i = 0; i < arr.size(); i++)
+		{
+			std::cout << " " << arr[i];
+		}
+		std::cout << std::endl;
+	}
 	/*
-	 *	Assertion sort algorithm.
-	 *	Template.
-	 *	Returns pointer
+	 *	Insertion sort algorithm.
+	 *	Template
+	 *	time complexity = O(n^2)
+	 *	space complexity = O(1)
 	 */
 	template<class T>
-	void insertion(T* arr, int n)
+	static void insertion(T* arr, int n)
 	{
 		//Iterate through each component in the passed array
 		//Forget about the first component (0)
@@ -43,7 +66,7 @@ public:
 	}
 
 	template<class T>
-	void insertion(std::vector<T> arr)
+	static void insertion(std::vector<T> &arr)
 	{
 		for(int i = 1; i < arr.size(); ++i)
 		{
@@ -55,6 +78,53 @@ public:
 				j = j - 1;
 			}
 			arr[j+1] = tmp;
+		}
+	}
+
+	/*
+	 *  Selection Sort Algorithm
+	 *	Template
+	 *	time complexity = O(n^2)
+	 *	space complexity = O(1)
+	 */
+	template<class T>
+	static void selection(T* arr, int n)
+	{
+		//iterate through the array
+		for(int i = 0; i < n - 1; i++)
+		{
+			//find minimum in the unsorted array
+			int min_idx = i;
+			for(int j = i; j < n; j++)
+			{
+				if(arr[j] < arr[min_idx])
+				{
+					min_idx = j;
+				}
+			}
+
+			std::swap(&arr[min_idx], &arr[i]);
+		}
+	}
+
+	template<class T>
+	static void selection(std::vector<T> &arr)
+	{
+		for(int i = 0; i < arr.size(); ++i)
+		{
+
+			//find minimum
+			int min_idx = i;
+			for(int j = i; j < arr.size(); ++j)
+			{
+				if(arr[j] < arr[min_idx])
+				{
+					min_idx = j;
+				}
+			}
+			T tmp = arr[i];
+			arr[i] = arr[min_idx];
+			arr[min_idx] = tmp;
 		}
 	}
 };
