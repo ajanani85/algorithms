919c02c0a2926831ca60bd1db987922fe190e123
ajanani85 <ajanani85@gmail.com>
2020-10-20 09:24:02 -0700

    updates

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -3,29 +3,99 @@ using namespace std;
 
 struct LinkedList
 {
+	LinkedList(int d)
+	{
+		data = d;
+		next = NULL;
+		size = 1;
+	}
+	LinkedList(LinkedList *item)
+	{
+		data = item->data;
+		next = item->next;
+		size = item->size;
+	}
 	int data;
 	LinkedList *next;
+	size_t size;
 };
 
-LinkedList* push_back(LinkedList *head, int data)
+/**
+ * reverse the order of the passed LinkedList
+ */
+void reverse(LinkedList **head)
 {
-	if(head == NULL)
+}
+
+/**
+ * insert an item at the given index
+ */
+void insert(LinkedList **head, int data, int index)
+{
+}
+
+/**
+ * insert an item in order
+ */
+void insert(LinkedList **head, int data)
+{
+	if(*head == NULL)
 	{
-		head = new LinkedList();
-		head->data = data;
-		return head;
+		*head = new LinkedList(data);
+		return;
 	}
 
-	LinkedList *current = head;
+	LinkedList *current = *head;
+	while(current->next != NULL || current->data < data)
+	{
+		current = current->next;
+	}
+
+	LinkedList *item = new LinkedList(data);
+	item->next = current->next;
+	current->next = item;
+}
+
+/**
+ * push the passed item in the front
+ */
+void push_front(LinkedList **head, int data)
+{
+	if(*head == NULL)
+	{
+		*head = new LinkedList(data);
+		return;
+	}
+
+	LinkedList *tmp = new LinkedList(*head);
+
+	LinkedList *current = new LinkedList(data);
+	current->next = tmp;
+	current->size = (*head)->size + 1;
+
+	*head = current;
+	//(*head)->size++;
+}
+
+void push_back(LinkedList **head, int data)
+{
+	if(*head == NULL)
+	{
+		*head = new LinkedList(data);
+		return;
+	}
+
+	//Move the cursor to the end of the array
+	LinkedList *current = *head;
 	while(current->next != NULL)
 	{
 		current = current->next;
 	}
 
-	LinkedList *item = new LinkedList();
-	item->data = data;
+	//create a new item and add it the current cursor position
+	LinkedList *item = new LinkedList(data);
 	current->next = item;
-	return head;
+	(*head)->size++;
 }
 
 void print(LinkedList *head)
@@ -48,9 +118,12 @@ void print(LinkedList *head)
 int main(int argc, char **argv) {
 
 
-	LinkedList *head;
+	LinkedList *head = NULL;
 
-	push_back(head, 10);
+	for(int i = 1; i <= 10; i++)
+	{
+		insert(&head, i * 100 / i);
+	}
 
 	print(head);
 
