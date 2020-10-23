0ae7d910e91128aa6bbe03fb68e9a009c29625c8
ALIREZA JANANI <ajanani85@gmail.com>
2020-10-23 06:17:49 -0700

    feat: updates

--- 2d_array/2d_array.cpp
+++ 2d_array/2d_array.cpp
@@ -4,15 +4,116 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-void create(int ***arr, int row, int col)
+void stringTo2DArray(const std::string &map, int ***arr, int row, int col);
+void create(int ***arr, int row, int col, int default_value);
+void print(int **arr, int row, int col);
+void pathfinder(int **arr, int row, int col, int start, int end);
+void findPaths(int **arr, int row, int col, int start_r, int start_c, int end, std::vector<int> &path);
+
+void findPaths(int **arr, int row, int col, int start_r, int start_c, int end, std::vector<int> &path)
+{
+	if(start_r*col + start_c == end)
+	{
+		return;
+	}
+
+	if(arr[std::min(start_r+1, row -1 )][start_c] == 1)
+	{
+		int idx = std::min(start_r+1, row -1)*col + (start_c);
+		if(idx == end)
+		{
+			path.push_back(idx);
+			return;
+		}
+
+		path.push_back(idx);
+		findPaths(arr, row, col, std::min(start_r+1, row -1), start_c, end, path);
+	}
+	if(arr[start_r][std::min(start_c+1, col-1)] == 1)
+	{
+		int idx = start_r*col + (std::min(start_c+1, col-1));
+		if(idx == end)
+		{
+			path.push_back(idx);
+			return;
+		}
+
+		path.push_back(idx);
+		findPaths(arr, row, col, start_r, std::min(start_c+1, col-1), end, path);
+	}
+}
+
+void pathfinder(int **arr, int row, int col, int start, int end)
+{
+	if(arr == NULL)
+	{
+		cout << "no path is found" << endl;
+	}
+	std::vector<std::vector<int>> paths;
+	std::vector<int> path;
+	path.push_back(start);
+	findPaths(arr, row, col, start/col, start%col, end, path);
+
+	for_each(path.begin(), path.end(), [&](int x){cout << x << " ";});
+	cout << endl;
+
+}
+
+void stringTo2DArray(const std::string &map, int ***arr, int row, int col)
+{
+	if((*arr) == NULL)
+	{
+		create(arr, row, col, 0);
+	}
+
+	int length = map.size();
+	for(int i = 0; i < map.length(); i++)
+	{
+		int val = map[i] - '0';
+
+		int r = i / col;
+		int c = i % col;
+		(*arr)[r][c] = val;
+	}
+	cout << endl;
+}
+
+void create(int ***arr, int row, int col, int default_value)
 {
 	(*arr) = new int*[row];
 	for(int i = 0; i < row; i++)
 	{
 		(*arr)[i] = new int[col];
 	}
+
+	for(int r = 0; r < row; r++)
+	{
+		for(int c = 0; c < col; c++)
+		{
+			(*arr)[r][c] = default_value;
+		}
+	}
+}
+
+void print(int **arr, int row, int col)
+{
+	if(arr == NULL)
+	{
+		return;
+	}
+
+	for(int r = 0; r < row; r++)
+	{
+		for(int c = 0; c < col; c++)
+		{
+			cout << arr[r][c] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 void printArr2d(const std::vector<std::vector<int>> &src)
@@ -168,7 +269,8 @@ void cluster(const std::vector<std::vector<int>> &src, std::vector<std::vector<i
 
 int main(int argc, char **argv) {
 	int **arr = NULL;
-	int row = 4, col = 4;
+	int row = 4, col = 4, default_val = 0;
+
 	/*std::vector<std::vector<int>> arr = {{1, 1, 0, 0, 1, 0, 0, 1},
 										 {1, 1, 0, 0, 1, 0, 0, 1},
 										 {0, 0, 0, 0, 1, 1, 1, 1},
@@ -181,6 +283,15 @@ int main(int argc, char **argv) {
 	}
 
 	cluster(arr, arr_labeled);*/
-	create(&arr, row, col);
+	create(&arr, row, col, default_val);
+
+	std::string map_str="1000100010001111";
+
+	stringTo2DArray(map_str, &arr, row, col);
+
+	pathfinder(arr, row, col, 0, (row*col)-1);
+
+	print(arr, row, col);
+
 	return 0;
 }
